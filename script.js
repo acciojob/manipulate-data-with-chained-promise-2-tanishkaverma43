ad04a0cfaf64faa4cfe2e2fec57b630d6ef5abbe
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-03-03 16:49:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 let promise = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve([1, 2, 3, 4]);
-    }, 3000);
+    }, 0);
 });
 
 promise.then((array) => {
-    let filteredArray = array.filter(num => num % 2 === 0);
-    document.getElementById('output').innerText = filteredArray.toString();
-    return filteredArray;
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let filteredArray = array.filter(num => num % 2 === 0);
+            document.getElementById('output').innerText = filteredArray.toString();
+            resolve(filteredArray);
+        }, 1000);
+    });
 }).then((array) => {
-    let doubledArray = array.map(num => num*2);
-    document.getElementById('output').innerText = doubledArray.toString();
-    return doubledArray;
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            let doubledArray = array.map(num => num*2);
+            document.getElementById('output').innerText = doubledArray.toString();
+            resolve(doubledArray);
+        }, 2000);
+    });
 });
