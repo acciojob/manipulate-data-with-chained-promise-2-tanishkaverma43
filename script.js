68f00de82f022752edac88e3b0b7f12fb4d399ef
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-03-03 16:45:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your JS code here. If required.
+let promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve([1, 2, 3, 4]);
+    }, 3000);
+});
+
+promise.then((array) => {
+    return array.filter(num => num % 2 === 0);
+}).then((array) => {
+	return array.map(num => num*2);
+});
